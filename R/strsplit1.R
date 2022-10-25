07026ecbbe86dc5a6b792293d52e22691b8b67df
treynolds <treynolds@victoryrenewables.com>
2022-10-25 15:41:49 -0500

    Altered example in new file to remove error

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -9,10 +9,6 @@
 #' @examples
 #' x <- "alfa,bravo,charlie,delta"
 #' strsplit1(x, split = ",")
-#' strsplit1(x, split = ",")
-#'
-#' y <- "192.168.0.1"
-#' strsplit1(y, pattern = stringr::fixed("."))
 
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
